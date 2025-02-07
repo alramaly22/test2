7595645e4ff1f88616fbc7b8e09d5c002c008ea3
m.alramaly <96423919+alramaly22@users.noreply.github.com>
2025-02-07 18:55:34 +0200

    Add files via upload

--- myproject/settings.py
+++ myproject/settings.py
@@ -81,11 +81,34 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# DATABASES = {
+
+#     'default': {
+
+
+#         'ENGINE': 'django.db.backends.postgresql',
+
+#         'NAME': 'railway',
+
+#         'USER': 'postgres',
+
+#         'PASSWORD': 'CmCCbvbOCIOgmxbqmESqxxYUCcNErdRB',
+
+#         'HOST': 'viaduct.proxy.rlwy.net',  # اسم المضيف
+#         'PORT': '47172',
+
+#     }
+
+
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 # Password validation
